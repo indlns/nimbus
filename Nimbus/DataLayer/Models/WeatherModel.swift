225b21cd81486ced7edecb688fbb7afff090450f
Evgenij Ovchinnikov <timlamb146@gmail.com>
2025-10-26 19:38:31 +0500

    add api service && model

---
+++ Nimbus/DataLayer/Models/WeatherModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherModel.swift
+//  Nimbus
+//
+//  Created by Евгений Овчинников on 26.10.2025.
+//
+
+import Foundation
+
---
+++ Nimbus/DataLayer/Network/ApiServices.swift
@@ -0,0 +1,16 @@
+//
+//  ApiServices.swift
+//  Nimbus
+//
+//  Created by Евгений Овчинников on 26.10.2025.
+//
+
+import Foundation
+
+class ApiServices {
+    let shared = ApiServices()
+    
+    func getWeatherData(completion : @escaping (ToDoListModel) -> ()) {
+        
+    }
+}
